3c134ed47b76188a2bc47ec4da8435df3693e05c
maple108 <61077504@qq.com>
2020-03-20 15:25:04 +0800

    test0320-2

--- src/cn/sunhu/JustTest.java
+++ src/cn/sunhu/JustTest.java
@@ -2,6 +2,6 @@ package cn.sunhu;
 
 public class JustTest {
     public static void main(String[] args) {
-        System.out.println("hello!GitHub!!");
+        System.out.println("hello!GitHub!!!");
     }
 }
